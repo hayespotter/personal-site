ca052674690e94df0fdd7542b04bd0e9c8f7cb48
Hayes Potter <hayes@riversagency.com>
2017-07-10 19:11:11 -0400

    bootstrap

---
+++ index.php
@@ -0,0 +1,14 @@
+<!doctype html>
+<html amp lang="en">
+  <head>
+    <meta charset="utf-8">
+    <title>Hayes Potter | System Administrator, IT Consultant</title>
+    <link rel="canonical" href="/index.html">
+    <meta name="viewport" content="width=device-width,minimum-scale=1,initial-scale=1">
+    <style><?php readfile( getcwd() . "/style.css"); ?></style>
+    <script async src="https://cdn.ampproject.org/v0.js"></script>
+  </head>
+  <body>
+    
+  </body>
+</html>
