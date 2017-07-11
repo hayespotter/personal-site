4122690d50b8282c7983a8dc78892ff0aa0928b7
Hayes Potter <hayes@riversagency.com>
2017-07-11 18:04:01 -0400

    header stuff

--- index.php
+++ index.php
@@ -1,14 +1,27 @@
 <!doctype html>
 <html amp lang="en">
-  <head>
-    <meta charset="utf-8">
-    <title>Hayes Potter | System Administrator, IT Consultant</title>
-    <link rel="canonical" href="/index.html">
-    <meta name="viewport" content="width=device-width,minimum-scale=1,initial-scale=1">
-    <style><?php readfile( getcwd() . "/style.css"); ?></style>
-    <script async src="https://cdn.ampproject.org/v0.js"></script>
-  </head>
-  <body>
+    <head>
+        <meta charset="utf-8">
+        <title>Hayes Potter | System Administrator, IT Consultant</title>
+        <link rel="canonical" href="/index.html">
+        <meta name="viewport" content="width=device-width,minimum-scale=1,initial-scale=1">
+        <link href="https://fonts.googleapis.com/css?family=Open+Sans:300,400,700|PT+Mono" rel="stylesheet">
+        <style><?php readfile( getcwd() . "/style.css"); ?></style>
+        <script async src="https://cdn.ampproject.org/v0.js"></script>
+    </head>
+
+<body>
+
+<header>
+    <div class="container">
+        <h1>Hayes Potter <span>| System Administrator &amp; I.T. Consultant</span></h1>
+    </div><!-- /.container -->
     
-  </body>
+</header>
+
+
+    
+</body>
+
+
 </html>
